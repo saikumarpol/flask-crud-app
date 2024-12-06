b096ef04fd2a8865a47986918d9b62b7d17d025a
saikumarpol <saikumar.pola@research.iiit.ac.in>
2024-12-06 23:32:07 +0530

    Initial commit of Flask CRUD app

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Dummy data for users
-users = ["John Doe", "Jane Smith", "Alice Johnson"]
-
 @app.route('/')
-def index():
-    return render_template('index.html', users=users)
-
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    # Get the username from the form
-    new_user = request.form.get('username')
-    if new_user:
-        users.append(new_user)
-    return redirect(url_for('index'))
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
